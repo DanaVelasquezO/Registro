0d56ecf223d80ebd0114ebbe2f84c4e5d7564bc2
DanaVelasquezO <danacarol.velasquez@gmail.com>
2025-11-29 00:31:47 -0500

    cambios no relevantes, eliminacion de una linea en blanco

--- db/sql/inserciones.sql
+++ db/sql/inserciones.sql
@@ -4,7 +4,6 @@ INSERT INTO Docente (Nombre_docente)
 VALUES ('Roxana Salazar');
 
 INSERT INTO Usuarios (usuario, clave, rol, codigo_docente)
-VALUES ('SistemAdmin', '@Sis4dmin.', 'admin', NULL);
-
-INSERT INTO Usuarios (usuario, clave, rol, codigo_docente)
 VALUES ('rox.salazar', 'R0x@n4.2025', 'docente', LAST_INSERT_ID());
+INSERT INTO Usuarios (usuario, clave, rol, codigo_docente)
+VALUES ('SistemAdmin', '@Sis4dmin.', 'admin', NULL);
